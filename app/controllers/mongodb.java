e6bde6b37cfe40529edd751b598ed37ae11f3145
Phoenix-Chen <phoenix0722chen@gmail.com>
2016-02-21 04:00:21 -0800

    added username

--- app/controllers/mongodb.java
+++ app/controllers/mongodb.java
@@ -1,3 +1,4 @@
+package controllers;
 /**
  * Created by JASON on 2/20/16.
  */
@@ -11,47 +12,48 @@ import java.util.Iterator;
 
 public class mongodb
 {
-    static MongoDatabase db;
+    static MongoClient mongoClient = new MongoClient("localhost", 27017);
+    static MongoDatabase db = mongoClient.getDatabase("Foober");
 
-    public static void main(String args[]) {
-        MongoClient mongoClient = new MongoClient("localhost", 27017); //Exception
-
-        //@SuppressWarnings("deprecation")
-        db = mongoClient.getDatabase("Foober");
-
-        for (String name: db.listCollectionNames()) {
-            System.out.println("Collection Name: " + name);
-            MongoCollection<Document> coll = db.getCollection(name);
-            System.out.println("# of Data in Collection: " + coll.count());
-        }
-
-        System.out.println("demonstrating find(collection, id)");
-        System.out.println(find("users", "56c95f31dbe73c089dbdc992"));
-        System.out.println("###########");
-
-        System.out.println("demonstrating find(collection)");
-        Iterator<Document> i = find("users").iterator();
-        while(i.hasNext())
-        {
-            System.out.println(i.next().toJson());
-        }
-        System.out.println("###########");
-
-        System.out.println("demonstrating findPosts(cook_id)");
-        Iterator<Document> iterator = findPosts("56c94e8445b863f3bafd3c09").iterator();
-        while(iterator.hasNext())
-        {
-            System.out.println(iterator.next().toJson());
-        }
-        System.out.println("###########");
-
-    }
+//    public static void main(String args[]) {
+//        MongoClient mongoClient = new MongoClient("localhost", 27017); //Exception
+//
+//        //@SuppressWarnings("deprecation")
+//        db = mongoClient.getDatabase("Foober");
+//
+//        for (String name: db.listCollectionNames()) {
+//            System.out.println("Collection Name: " + name);
+//            MongoCollection<Document> coll = db.getCollection(name);
+//            System.out.println("# of Data in Collection: " + coll.count());
+//        }
+//
+//        System.out.println("demonstrating find(collection, id)");
+//        System.out.println(find("users", "56c95f31dbe73c089dbdc992"));
+//        System.out.println("###########");
+//
+//        System.out.println("demonstrating find(collection)");
+//        Iterator<Document> i = find("users").iterator();
+//        while(i.hasNext())
+//        {
+//            System.out.println(i.next().toJson());
+//        }
+//        System.out.println("###########");
+//
+//        System.out.println("demonstrating findPosts(cook_id)");
+//        Iterator<Document> iterator = findPosts("56c94e8445b863f3bafd3c09").iterator();
+//        while(iterator.hasNext())
+//        {
+//            System.out.println(iterator.next().toJson());
+//        }
+//        System.out.println("###########");
+//
+//    }
 
     //find that specifies the collection("cooks", "users", or "posts") and id(a String)
-    public static String find(String collection, String id)
+    public static Document find(String collection, String id)
     {
         MongoCollection<Document> coll = db.getCollection(collection);
-        return coll.find(new Document("_id", id)).first().toJson();
+        return coll.find(new Document("_id", id)).first();
     }
 
     //find all the data of a collection("posts", "users", or "cooks")
--- public/javascripts/dishes.js
+++ public/javascripts/dishes.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
             var cardDeck = document.createElement('div');
             cardDeck.className="card-deck";
             for(var i=0;i<obj.length;i++){
-                cardDeck.innerHTML=cardDeck.innerHTML+'<div class="card"><a href="#" onclick=\'setModals(\"'+obj[i].title+'\",\"'+obj[i].price+'\",\"'+obj[i].description+'\",\"'+obj[i].ingradient+'\")\' data-toggle="modal" data-target="#foodModal"><img class="card-img-top img-responsive" src="http://placehold.it/150x80?text=IMAGE" style="width:100%" alt="Food pic"></a><div class="card-block"><div class="row"><div class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><div class="short-div foodName">'+obj[i].title+'</div><div class="short-div foodDesc">'+obj[i].description+'</div></div><div class="col-lg-4 col-md-4 col-sm-6 col-xs-4 price">$'+obj[i].price+'.00</div></div><div class="row"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><div class="pull-right"><button type="button" class="btn btn-success-outline">Order now</button></div></div></div><br><div class="row"><p class="card-text"><small class="text-muted pull-right">*May contain peanuts and dairy products.</small></p></div></div></div>';
+                cardDeck.innerHTML=cardDeck.innerHTML+'<div class="card"><a href="#" onclick=\'setModals(\"'+obj[i].title+'\",\"'+obj[i].cook_id+'\",\"'+obj[i].price+'\",\"'+obj[i].description+'\",\"'+obj[i].ingradient+'\")\' data-toggle="modal" data-target="#foodModal"><img class="card-img-top img-responsive" src="http://placehold.it/150x80?text=IMAGE" style="width:100%" alt="Food pic"></a><div class="card-block"><div class="row"><div class="col-lg-8 col-md-8 col-sm-8 col-xs-8"><div class="short-div foodName">'+obj[i].title+'</div><div class="short-div foodDesc">'+obj[i].description+'</div></div><div class="col-lg-4 col-md-4 col-sm-6 col-xs-4 price">$'+obj[i].price+'.00</div></div><div class="row"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><div class="pull-right"><button type="button" class="btn btn-success-outline">Order now</button></div></div></div><br><div class="row"><p class="card-text"><small class="text-muted pull-right">*May contain peanuts and dairy products.</small></p></div></div></div>';
                 
                 
                 if (i%3==2) {
@@ -20,8 +20,13 @@ $(document).ready(function(){
     });
 });
 
-function setModals(title, price,description,ingradients) {
+function setModals(title, cId,price,description,ingradients) {
     $('#modal-title').html(title);
+    myJsRoutes.controllers.Application.getCookNameById(cId).ajax({
+        success : function(cookName) {
+            $('#modal-provider').html("Prepared by:"+cookName);
+        }
+        });
     $('#modal-price').html("$"+price+".00");
     $('#modal-description').html(description);
     $('#modal-ingradients').html(ingradients)
